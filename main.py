4ef3f490c86217a4b41c137bb359f8f22e70b2a0
daiton <daitoncheis@gmail.com>
2025-06-06 04:29:06 -0300

    update de comandos

--- main.py
+++ main.py
@@ -16,16 +16,24 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 async def on_ready():
     print(f'Bot conectado com sucesso como :{bot.user}')
 
-@bot.command(name='oi') #!hello 
-async def oi(ctx):
+@bot.command(name='ola', aliases=["oi", "Oi", "OI", "oii", "Oii", "OII", "oiii", "Oiii", "OIII", "Olá", "OLÁ", "olá",
+                                  "Olá!", "olá!", "Ola", "OLA", "olarr", "Olarr", "OLARR", "eae", "Eae", "EAE", "e aí", 
+                                  "E aí", "E AÍ", "e ai", "E ai", "E AI", "opa", "Opa", "OPA", "fala", "Fala", "FALA",
+                                  "alô", "Alô", "ALÔ", "alo", "Alo", "ALO", "salve", "Salve", "SALVE", "hello", "Hello", 
+                                   "HELLO", "hellos", "Hellos", "HELLOS", "hallo", "Hallo", "HALLO",])  # Comando .ola, .oi ou .olá
+async def ola(ctx):
     await ctx.send('Olá! Muito prazer, me chamo Juninho da pika laranja.')
 
 @bot.command(name='ping')
 async def ping(ctx):
     latency = round(bot.latency * 1000)  # Latência em milissegundos
     await ctx.send(f'Pong! Latência: {latency}ms')
+    
+@bot.command(name="eco", help="Repete o que você diz.")
+async def echo(ctx, *, message: str):
+    await ctx.send(message)
 
-@bot.command(name='comandos')
+@bot.command(name='help', aliases=['ajuda', 'comandos'])
 async def teste(ctx):
     await ctx.send('Lista de comandos!')
 bot.run(TOKEN)
